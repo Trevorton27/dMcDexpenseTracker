6727bd084c8f3a273851fc39ac85fb1855295cd6
Duane McDonald <mcdonald.duane13@gmail.com>
2021-03-10 21:19:07 -0500

    WIP build row

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-// expense tracker logic
+const submitBtn = document.querySelector("button");
+submitBtn.addEventListener("click", () => {
+    createExpenseRow();
+})
+
+function createExpenseRow () {
+    const tracker = document.querySelector(".table tbody");
+    const template = document.querySelector('template');
+    const newRow =  template.content.cloneNode(true);
+    const newDate = document.querySelector("input[name='date']");
+    newRow.querySelector('#date').value = newDate.value
+    const newName = document.querySelector("input[name='name']");
+    newRow.querySelector('#name').value = newName.value
+    // tracker.append(newRow);
+    console.log(newRow);
+}
+
+// document.addEventListener('DOMContentLoaded', APP.init());
