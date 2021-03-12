c6989fc9f84041d2dbbfe6627b985586a753c70f
Duane McDonald <mcdonald.duane13@gmail.com>
2021-03-12 11:06:40 -0500

    WIP delete row

--- app.js
+++ app.js
@@ -1,18 +1,59 @@
-const submitBtn = document.querySelector("button");
-submitBtn.addEventListener("click", () => {
-    createExpenseRow();
-})
+const APP = {
+    submitBtn: document.querySelector("button"),
+    table: document.querySelector("table"),
+    date: document.querySelector('.dateInput'),
+    expName: document.querySelector('.nameInput'),
+    vendor: document.querySelector('.vendorInput'),
+    type: document.querySelector('.typeInput'),
+    amount: document.querySelector('.amountInput'),
+    description: document.querySelector('.descriptionInput'),
+    deleteButton: document.querySelector('.delete'),
 
-function createExpenseRow () {
-    const tracker = document.querySelector(".table tbody");
-    const template = document.querySelector('template');
-    const newRow =  template.content.cloneNode(true);
-    const newDate = document.querySelector("input[name='date']");
-    newRow.querySelector('#date').value = newDate.value
-    const newName = document.querySelector("input[name='name']");
-    newRow.querySelector('#name').value = newName.value
-    // tracker.append(newRow);
-    console.log(newRow);
+    init () {
+        resetInputs();
+        APP.submitBtn.addEventListener("click", () => {
+            createExpenseRow();
+        });
+    }
 }
-
-// document.addEventListener('DOMContentLoaded', APP.init());
+function dateTimeDisplay() {
+    const dateTime = new Date();
+    dateTime.setMinutes(dateTime.getMinutes() - dateTime.getTimezoneOffset());
+    dateTime.setMilliseconds(null);
+    dateTime.setSeconds(null);
+    document.querySelector('.dateInput').value = dateTime.toISOString().slice(0, -1);
+}
+function resetInputs (){
+    dateTimeDisplay();
+    APP.expName.value = "";
+    APP.vendor.value = "";
+    APP.type.value = "";
+    APP.amount.value = "";
+    APP.description.value = "";
+}
+function createExpenseRow () {
+    i = APP.table.rowIndex
+    const template = document.querySelector('template');
+    const content = template.content.cloneNode(true)
+    const deleteButton = document.querySelector('.delete')
+    content.querySelector('#dateTemp').textContent = APP.date.value
+    content.querySelector('#nameTemp').textContent = APP.expName.value
+    content.querySelector('#vendorTemp').textContent = APP.vendor.value
+    content.querySelector('#typeTemp').textContent = APP.type.value
+    content.querySelector('#amountTemp').textContent = APP.amount.value
+    content.querySelector('#descriptionTemp').textContent = APP.description.value
+    document.getElementsByClassName('delete').innerHTML = deleteButton
+    APP.table.appendChild(content);
+    deleteButton.addEventListener("click", (e) => {
+        // deleteRow(e);
+        console.log("duane is awesome!")
+    })
+    resetInputs();
+}
+function deleteRow(e) {
+    var i = e.parentNode.parentNode.rowIndex;
+    APP.table.deleteRow(i);
+}
+    //localStorage.removeItem(key);
+    
+document.addEventListener('DOMContentLoaded', APP.init());
